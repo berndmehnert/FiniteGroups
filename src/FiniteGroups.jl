50909fdd3c914674491e5ccb80f45bfc2e588f20
bernd.h.mehnert <bernd.h.mehnert@gmail.com>
2020-09-06 15:33:17 +0200

    cycle equality

--- src/FiniteGroups.jl
+++ src/FiniteGroups.jl
@@ -1,5 +1,5 @@
 module FiniteGroups
-import Base.map, Base.*, Base.^
+import Base.map, Base.*, Base.^, Base.==
 export τ, Cyc, Perm, Permutation
 
 """
@@ -26,6 +26,7 @@ struct Perm <: Permutation
     arr :: Array{Int64,1}
     Perm(arr) = length(Set(arr)) != length(arr) ? error("Elements in array need to be pairwise distinct!") : new(arr)
 end
+
 """
 Define a cycle:
 
@@ -37,21 +38,30 @@ struct Cyc <: Permutation
     dict :: Dict{Int64, Int64}
 end
 
-function Cyc(ns :: Int64 ...)
-    if length(Set(ns)) != length(ns)
+struct Perm2 <: Permutation
+    cycles :: Vector{Cyc}
+    key_to_cycle_map 
+end
+
+function Cyc(v :: Vector{Int64})
+    if length(Set(v)) != length(v)
         error("Elements in list have to be distinct!")
     end
-    if length(ns) < 2
+    if length(v) < 2
         return Cyc(Dict([]))
     end
     dict_arr = []
-    for i in 1:length(ns)-1
-        push!(dict_arr, (ns[i], ns[i+1]))
+    for i in 1:length(v)-1
+        push!(dict_arr, (v[i], v[i+1]))
     end
-    push!(dict_arr, (ns[end], ns[1]))
+    push!(dict_arr, (v[end], v[1]))
     return Cyc(Dict(dict_arr))
 end
 
+Cyc(ns :: Int64 ...) = Cyc(collect(ns))
+
+==(σ :: Cyc, τ :: Cyc) = σ.dict == τ.dict
+ 
 function *(σ :: Cyc, τ :: Cyc)
     return 1
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -12,3 +12,11 @@ end
     @test map(σ, 3) == 3
     @test map(σ, 2) == 5
 end
+
+@testset "cycles + relations" begin
+    σ = Cyc([1,2,3])
+    τ = Cyc(2,3,1)
+    γ = Cyc(1,3,2)
+    @test σ == τ
+    @test !(σ == γ)
+end
